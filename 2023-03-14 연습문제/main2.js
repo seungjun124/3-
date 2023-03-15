efc6d81158be5ba9ad0c25c05794dc267fdaba97
seungjun124 <127902249+seungjun124@users.noreply.github.com>
2023-03-15 11:08:51 +0900

    Update main2.js

--- 2023-03-14 연습문제/main2.js
+++ 2023-03-14 연습문제/main2.js
@@ -2,25 +2,19 @@ var [text, text2] = prompt("두 숫자를 입력하세요. 예시)100 200").spli
 let arr = [];
 let arr2 = [];
 let arr3 = [];
-let sum = -1;
-let sum2 = -1;
-let sum3 = -1;
 
 for (let i = 1; i <= Math.max(text, text2); i++) {
     if (text % i === 0) {
-        sum++;
         arr.push(i);
     }
 
     if (text2 % i === 0) {
-        sum2++;
         arr2.push(i);
     }
 
     if (text % i === 0 && text2 % i === 0) {
-        sum3++;
         arr3.push(i);
     }
 }
 
-document.write(`${text}모든 요인: ${arr}<br>${text2}모든 요인: ${arr2}<br>최대 공약수는 다음과 같습니다.${arr3[sum3]}`)
+document.write(`${text}모든 요인: ${arr}<br>${text2}모든 요인: ${arr2}<br>최대 공약수는 다음과 같습니다.${arr3[arr3.length-1]}`)
