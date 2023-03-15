90f54e31623f4c158bf7a286c392d28a6bca375b
seungjun124 <127902249+seungjun124@users.noreply.github.com>
2023-03-15 10:18:03 +0900

    Add files via upload

--- 2023-03-14 연습문제/main2.js
+++ 2023-03-14 연습문제/main2.js
@@ -1,12 +1,27 @@
 let text = parseInt(prompt('숫자를 입력하세요.'));
 let text2 = parseInt(prompt('숫자를 입력하세요.'));
+let arr = [];
+let arr2 = [];
+let arr3 = [];
+let sum = -1;
+let sum2 = -1;
+let sum3 = -1;
 
-for (let i = 2; i <= Math.min(text, text2); i++) {
-    if (text % 2 == 0 && text2 % 2 == 0) {
-        alert(i);
-        break;
-    } else {
-        alert("1");
-        break;
+for (let i = 1; i <= Math.max(text, text2); i++) {
+    if (text % i === 0) {
+        sum++;
+        arr.push(i);
+    }
+
+    if (text2 % i === 0) {
+        sum2++;
+        arr2.push(i);
+    }
+
+    if (text % i === 0 && text2 % i === 0) {
+        sum3++;
+        arr3.push(i);
     }
 }
+
+document.write(`${text}모든 요인: ${arr}<br>${text2}모든 요인: ${arr2}<br>최대 공약수는 다음과 같습니다.${arr3[sum3]}`)
